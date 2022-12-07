6daab3b825b57d483495edec6ba4bb2dae69971e
ashiba <ashibasama@gmail.com>
2022-12-07 20:14:26 +0900

    first-c

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品管理アプリ</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php">商品管理アプリ</a>
+        </nav>
+    <h1>商品管理アプリ</h1>
+    <p>『PHPとデータベースを連携しよう』成果物</p>
+    <a href="">商品一覧</a>
+</body>
+<footer>
+    <p>©︎商品管理アプリ ALL rights reserved.</p>
+</footer>
+</html>
